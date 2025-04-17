59baf627055bccba873db6f26ff3fc9e122d9483
Your Name <sushilphadtare2005@gmail.com>
2025-04-18 03:30:07 +0530

    Student Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import DashboardSuperAdmin from "./pages/SuperAdmin/DashboardSuperAdmin";
 import DashboardRector from "./pages/Rector/DashboardRector";
-import DashboardStudent from "./pages/Student/DashboardStudent";
+import StudentDashboard from "./pages/Student/DashboardStudent";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/superadmin" element={<DashboardSuperAdmin />} />
         <Route path="/rector" element={<DashboardRector />} />
-        <Route path="/student" element={<DashboardStudent />} />
+        <Route path="/student" element={<StudentDashboard />} />
       </Routes>
     </Router>
   );
